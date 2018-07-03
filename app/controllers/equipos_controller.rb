68098375434a5c5b483a0951c01fcff09957d0b9
clozano1991 <cristobalozano@gmail.com>
2018-07-03 00:02:58 -0400

    hitoDosFinalFinal

--- app/controllers/equipos_controller.rb
+++ app/controllers/equipos_controller.rb
@@ -1,2 +1,46 @@
 class EquiposController < ApplicationController
+	before_action :authenticate_user!
+	def index
+		@campo=current_user.campos.find(params[:campo_id])
+		@equipos=current_user.campos.find(params[:campo_id]).equipos.all
+	end
+    def show
+    	@campo=current_user.campos.find(params[:campo_id])
+    	
+    end
+
+	def create
+		@campo=current_user.campos.find(params[:campo_id])
+		@huerto=current_user.campos.find(params[:campo_id]).huertos.build(huerto_params)
+		@huerto.save
+		respond_to do|format|
+			format.html {redirect_to user_campo_huertos_path(current_user, @campo)}
+			format.js 
+		end
+	end
+
+	def update
+		@huerto=current_user.campos.find(params[:campo_id]).huertos.find(params[:id])
+		@huerto.update(huerto_params)
+		respond_to do|format|
+			format.html {redirect_to index_path}
+			format.js 
+		end
+	end
+
+	def destroy
+		@huerto=current_user.campos.find(params[:campo_id]).huertos.find(params[:id])
+	    @huerto.destroy
+	    respond_to do|format|
+			format.html {redirect_to index_path}
+			format.js 
+		end
+	end
+
+
+	private
+	
+	def equipo_params
+		params.require(:equipo).permit(:equipo, :marca, :modelo, :fechaFabricacion, :fechaAdquisicion, :estado)
+	end
 end
