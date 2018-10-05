bd6a78cf9c958f7ce6e062d8dff5ad1299fe6f58
clozano1991 <cristobalozano@gmail.com>
2018-10-05 11:41:47 -0300

    a

--- db/migrate/20180906210748_change_porcentaje_cotizacion_isapre_to_be_float.rb
+++ db/migrate/20180906210748_change_porcentaje_cotizacion_isapre_to_be_float.rb
@@ -2,4 +2,5 @@
 class ChangePorcentajeCotizacionIsapreToBeFloat < ActiveRecord::Migration[5.1]
   def change
   	change_column :empleados, :porcentajeCotizacionIsapre, using: "porcentajeCotizacionIsapre::float"
+  end
 end
