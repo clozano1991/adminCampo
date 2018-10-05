9fc93736f80f224b5b216d4a913320fd23615625
clozano1991 <cristobalozano@gmail.com>
2018-10-04 21:48:58 -0300

    a

--- db/migrate/20180906210748_change_porcentaje_cotizacion_isapre_to_be_double.rb
+++ db/migrate/20180906210748_change_porcentaje_cotizacion_isapre_to_be_double.rb
@@ -1,5 +1,5 @@
 
-class ChangePorcentajeCotizacionIsapreToBeDouble < ActiveRecord::Migration[5.1]
+class ChangePorcentajeCotizacionIsapreToBeFloat < ActiveRecord::Migration[5.1]
   def change
   	change_column :empleados, :porcentajeCotizacionIsapre, :float
   end
